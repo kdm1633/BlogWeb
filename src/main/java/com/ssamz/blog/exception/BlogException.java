273e317c5e1d3ee908708f12922408a8e666da53
kdm1633 <kdm1633@outlook.com>
2023-04-26 18:47:41 +0900

    Ch03 - JPA and MyBatis

---
+++ src/main/java/com/ssamz/blog/exception/BlogException.java
@@ -0,0 +1,9 @@
+package com.ssamz.blog.exception;
+
+public class BlogException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public BlogException(String message) {
+		super(message);
+	}
+}
